8b722ac4b85a448e1d7655297a9f78ac5d240bd1
vovareka <reus_10@ukr.net>
2020-06-16 22:29:54 +0300

    add dom

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import './scss/index.scss'
 import {Excel} from '@/components/excel/Excel';
 import {Header} from '@/components/header/Header';
 import {Toolbar} from '@/components/toolbar/Toolbar';
-import {Fotmula} from '@/components/formula/Fotmula';
+import {Formula} from '@/components/formula/Fotmula';
 import {Table} from '@/components/table/Table';
 
 const excel = new Excel('#app', {
-  components: [Header, Toolbar, Fotmula, Table]
+  components: [Header, Toolbar, Formula, Table]
 })
 
 excel.render()
